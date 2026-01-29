b8b08695f33fc56ceb61d01e5482a8895c353900
Miskachev Dima <miskachevds@yandex.ru>
2026-01-29 16:46:19 +0300

    card-start

---
+++ src/components/arrivals/Arrivals.jsx
@@ -0,0 +1,18 @@
+import './Arrivals.css';
+
+const Arrivals = () => {
+    return ( <section className="arrivals">
+        <div className="container">
+            <div className="arrivals_header">
+                <h2 className='title-2'>NEW ARRIVALS</h2>
+            </div>
+            <div className="arrivals_cards">
+                <div>Card1</div>
+                <div>Card2</div>
+                <div>Card3</div>
+            </div>
+        </div>
+    </section> );
+}
+ 
+export default Arrivals;
